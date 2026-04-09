66ee83ffdb2fb063fcddc86a1684a42b977390b6
Carlos de Anda <carlosfelipedeanda@gmail.com>
2026-04-09 00:41:30 -0600

    Fixed Projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,80 @@
-import {Link} from "react-router";
+import './Projects.css';
+import SmartMirror from '../assets/SmartMirror.jpeg';
+
 
 const Projects = () => {
-    return(
-     <>   
+  return (
+    <section className="projects-container">
+
+      <div className="projects-header">
         <h1>Projects</h1>
         <p>
-            <Link to = "/">Go Back</Link>
+          A selection of projects where I combine design, logic, and performance 
+          to build meaningful digital experiences.
         </p>
-    </>
-    )
-}
+      </div>
+
+      <div className="projects-grid">
+
+        <div className="project-card">
+          <img src={SmartMirror} alt="Smart Mirror" />
+
+          <h2>Smart Mirror</h2>
+          <p>
+            1st place Smart Mirror project at Expo Ingenierías. A full-stack IoT system combining computer vision, embedded devices, and real-time data to create a personalized interactive experience.
+          </p>
+
+          <a 
+            href="https://github.com/carlos-deanda/Smart-Mirror"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn"
+          >
+            View Project
+          </a>
+        </div>
+
+        <div className="project-card">
+          <div className="project-image"></div>
+
+          <h2>Netflix Simulation</h2>
+          <p>
+            Developed a modular C++ program simulating a streaming service with 50+ video objects, applying OOP
+principles for rating and filtering features
+          </p>
+
+          <a 
+            href="https://github.com/carlos-deanda/problem-situation-netflix-project"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn"
+          >
+            View Project
+          </a>
+        </div>
+
+        <div className="project-card">
+          <div className="project-image"></div>
+
+          <h2>Web Calculator</h2>
+          <p>
+            Functional web calculator built with JavaScript, supporting basic operations, decimals, negative values, and exponentiation. Includes DOM manipulation, regex-based input validation and a history feature for previous operations.
+          </p>
+
+          <a 
+            href="https://github.com/carlos-deanda/Projecto-1---Calculadora"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn"
+          >
+            View Project
+          </a>
+        </div>
+
+      </div>
+
+    </section>
+  );
+};
 
 export default Projects;
